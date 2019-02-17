0b0d9f41697085cafdcfe8f0a5c8cd0914634f0f
Manav-Aggarwal <manavaggarwal1234@gmail.com>
2019-02-17 09:04:59 -0800

    Added Twilio support

--- python/send_message.py
+++ python/send_message.py
@@ -2,10 +2,10 @@
 from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
-account_sid = 'ACaf6a830e53da8afedce6ea78eec8ccd2'
-auth_token = '263c9c80bfc9529ce440fa1c5862c598'
+account_sid = 'ACe644f8328e0ae4ffcdae613ce2be14f0'
+auth_token = '1a8bc584d9a6f708c0df200cf061a970'
 client = Client(account_sid, auth_token)
-from_ = '+12065382930'
+from_ = '+13476503435'
 
 def send_message(name, contact):
 	message = client.messages \
