5c03622be5a843a432b0618b5ceb47b157a6d394
Manan Rai <mananrai@stanford.edu>
2019-02-17 04:49:22 -0800

    Fix Graphs

--- python/app.py
+++ python/app.py
@@ -1,53 +1,55 @@
 #app.py
 import http.client, urllib.request, urllib.parse, urllib.error, json
 from os.path import isfile
-from flask import Flask, request, jsonify #import main Flask class and request object
+from flask import Flask, request, jsonify, make_response, render_template #import main Flask class and request object
 from flask_cors import CORS
 from send_message import send_message
 
-app = Flask(__name__) #create the Flask app
+app = Flask(__name__, template_folder='../templates') #create the Flask app #, template_folder='../public'
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SESSION_TYPE'] = 'filesystem'
 CORS(app)
-DATA_FILENAME = "data_sentiment.json"
-CONTACTS_FILENAME = "contacts.json"
+DATA_FILENAME = "/Users/mananrai/Desktop/Treehacks/2019/ours/my-app/python/data_sentiment.json"
+CONTACTS_FILENAME = "/Users/mananrai/Desktop/Treehacks/2019/ours/my-app/python/contacts.json"
 def getSentiments(inputJson):
-    headers = {
-        # Request headers
-        'Content-Type': 'application/json',
-        'Ocp-Apim-Subscription-Key': '91744f48b62b4c4ea23e4102d0fc282b',
-    }
+	headers = {
+		# Request headers
+		'Content-Type': 'application/json',
+		'Ocp-Apim-Subscription-Key': '91744f48b62b4c4ea23e4102d0fc282b',
+	}
 
-    params = urllib.parse.urlencode({
-    })
+	params = urllib.parse.urlencode({
+	})
 
-    try:
-        conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
-        conn.request("POST", "/text/analytics/v2.0/sentiment?%s" % params, inputJson, headers)
-        response = conn.getresponse()
-        data = json.loads(response.read()) 
-        conn.close()
-        textScoreList = data["documents"]
-        #addEmojis(textScoreList)
-        return textScoreList
-    except Exception as e:
-        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+	try:
+		conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
+		conn.request("POST", "/text/analytics/v2.0/sentiment?%s" % params, inputJson, headers)
+		response = conn.getresponse()
+		data = json.loads(response.read()) 
+		conn.close()
+		textScoreList = data["documents"]
+		#addEmojis(textScoreList)
+		return textScoreList
+	except Exception as e:
+		print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
 def getEmoji(score):
-    emoji = "invalid"
-    scoreBounds = {0.2: 'extreme-sad.png', 0.4: 'normal-sad.png', 0.6: 'neutral-face.png', 0.8: "normal-happy.png", 1: "extreme-happy.png"}
-    for scoreIdx in sorted(scoreBounds.keys()):
-        if(score <= scoreIdx):
-            emoji = scoreBounds[scoreIdx]
-            break
-    return emoji
+	emoji = "invalid"
+	scoreBounds = {0.2: 'extreme-sad.png', 0.4: 'normal-sad.png', 0.6: 'neutral-face.png', 0.8: "normal-happy.png", 1: "extreme-happy.png"}
+	for scoreIdx in sorted(scoreBounds.keys()):
+		if(score <= scoreIdx):
+			emoji = scoreBounds[scoreIdx]
+			break
+	return emoji
 
 def addEmojis(textScoreList):
-    for dictObj in textScoreList:
-        dictObj['emoji'] = getEmoji(dictObj['score'])
+	for dictObj in textScoreList:
+		dictObj['emoji'] = getEmoji(dictObj['score'])
 
 def addToList(textScoreList):
+	print("-" * 80)
 	print(textScoreList)
+	print("-" * 80)
 	with open(DATA_FILENAME, mode='a', encoding='utf-8') as dataFile:
 		for textScore in textScoreList:
 			dataFile.write(str(textScore['score']) + "\n")
@@ -65,19 +67,43 @@ def alert_contacts():
 					send_message(name, contact_2)
 		return "Success!"
 
-@app.route('/get_sentiment', methods=['GET', 'POST'])
-def get_sentiment():
+@app.route('/post_sentiment', methods=['POST'])
+def post_sentiment():
+	# python_data = {
+	#     'some_list': [4, 5, 6],
+	#     'nested_dict': {'foo': 7, 'bar': 'a string'}
+	# }
+	print("-" * 80)
+	print("post_sentiment")
+	print("-" * 80)
 	if request.method == 'POST':
 		req_data = request.get_json()
-		response = getSentiments(json.dumps(req_data))
+		print("-" * 80)
+		print("req data")
+		print("-" * 80)
+		response = getSentiments(req_data) #getSentiments(json.dumps(req_data))
 		addToList(response)
+		# print(response)
 		return jsonify(response)
-	elif request.method == 'GET':
+
+@app.route('/get_sentiment', methods=['GET'])
+def get_sentiment():
+	# python_data = {
+	#     'some_list': [4, 5, 6],
+	#     'nested_dict': {'foo': 7, 'bar': 'a string'}
+	# }
+	print("-" * 80)
+	print("get sentiment")
+	print("-" * 80)
+	if request.method == 'GET':
 		response = "invalid"
+		textScores = []
 		with open(DATA_FILENAME, mode='r', encoding='utf-8') as dataFile:
 			textScores = dataFile.read().split()
-			textScores = [float(textScore) for textScore in textScores]
-			response = jsonify(textScores)
-		return response
+			textScores = [100 * float(textScore) for textScore in textScores]
+			print(textScores)
+			return jsonify({"params": textScores})
+
+
 if __name__ == '__main__':
    app.run(debug=True, port=5000)
--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+// import './Chart.js'
+
+var Chart = require('chart.js');
 
 class App extends React.Component {
   constructor(props) {
@@ -77,18 +80,390 @@ class App extends React.Component {
     event.preventDefault();
   }
 
+  plotData(data_vals) {
+    // input = [40, 5, 70, 45, 50, 75, 90]
+
+    // data_vals = [input[0]]
+    // for(i = 0; i < input.length; i++) {
+    //  data_vals[i] = input[i]
+    // }
+    
+    // data_vals = input
+
+    var input = data_vals
+    
+    // data_vals[input.length + 1] = input[input.length-1]
+    console.log(data_vals)
+    var ctx = document.getElementById('myChart').getContext("2d");
+    var rect = document.getElementById('myChart').getBoundingClientRect();
+    console.log(rect.top, rect.right, rect.bottom, rect.left);
+
+    var gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
+    gradientStroke.addColorStop(1, '#80b6f4');
+    gradientStroke.addColorStop(0, '#f49080');
+
+    var x_happy = new Image();
+    var happy = new Image();
+    var sad = new Image();
+    var x_sad = new Image();
+    var neutral = new Image();
+
+    x_happy.src = "images/extreme-happy.png";
+    x_sad.src = "images/extreme-sad.png";
+    sad.src = "images/normal-sad.png";
+    happy.src = "images/normal-happy.png";
+    neutral.src = "images/neutral-face.png";
+    
+    var emotional_arr = [x_sad, sad, neutral, happy, x_happy]
+
+    //plot_points = ['rect', 'rect', x_happy, 'circle', 'rect', 'triangle', 'circle'];
+    // var chungus = new Image()
+  //    chungus.src ='chungus\ (1).png';
+      // chungus.style.width = '5px'
+      // chungus.style.height = '10px'
+    // var gradientFill = ctx.createLinearGradient(500, 0, 100, 0);
+
+    // plot_points = ["circle"]
+    var plot_points = []
+    for(var i = 0; i < input.length; i++) {
+      plot_points[i] = emotional_arr[parseInt((input[i]-1)/20)]
+    }
+    // plot_points[input.length+1] = "circle"
+    console.log(plot_points)
+    var gradientFill = ctx.createLinearGradient((rect.left+rect.right)/2, rect.top, (rect.left+rect.right)/2, rect.top + rect.bottom*3);
+    // gradientFill.addColorStop(0, "rgba(128, 182, 244, 0.6)");
+    // gradientFill.addColorStop(1, "rgba(244, 144, 128, 0.6)");
+
+
+    gradientFill.addColorStop(0, "rgba(244, 144, 128, 0.7)");
+    gradientFill.addColorStop(1, "rgba(128, 182, 244, 0.7)");
+
+    var offset = 50
+    Chart.pluginService.register({
+        afterUpdate: function(chart) {
+            // We get the dataset and set the offset here
+            var dataset = chart.config.data.datasets[0];
+
+            // For every data in the dataset ...
+            for (var i = 0; i < dataset._meta[0].data.length; i++) {
+                // We get the model linked to this data
+                var model = dataset._meta[0].data[i]._model;
+
+                // And add the offset to the `x` property
+                model.x += offset;
+
+                // .. and also to these two properties
+                // to make the bezier curve fits the new graph
+                model.controlPointNextX += offset;
+                model.controlPointPreviousX += offset;
+            }
+        }
+    });
+
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", ""],
+        // labels: ["f", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "f"],
+        datasets: [{
+          label: "Data",
+          borderColor: gradientStroke,
+          pointBorderColor: gradientStroke,
+          pointBackgroundColor: gradientStroke,
+          pointHoverBackgroundColor: gradientStroke,
+          pointHoverBorderColor: gradientStroke,
+          pointBorderWidth: 10,
+          pointHoverRadius: 10,
+          pointHoverBorderWidth: 1,
+          pointRadius: 3,
+          fill: true,
+          backgroundColor: gradientFill,
+          borderWidth: 10,
+          pointStyle: plot_points,
+          data: data_vals
+        }]
+      },
+      options: {
+        legend: {
+          position: "bottom"
+        },
+        layout: {
+              padding: {
+                  left: 50,
+                  right: 0,
+                  top: 0,
+                  bottom: 0
+              }
+          },
+        scales: {
+          yAxes: [{
+            ticks: {
+              fontColor: "rgba(0,0,0,0.5)",
+              fontStyle: "bold",
+              beginAtZero: true,
+              maxTicksLimit: 22,
+              padding: 60,
+              stepSize: 5,
+              fontSize: 60,
+              max: 100,
+              labelOffset: offset,
+              callback: function(label, index, labels) {
+                    switch (label) {
+                      // case 0:
+                      //  return ''
+                        case 10:
+                            return '😭 ';
+                        // case 20:
+                        //  return '';
+                        case 30:
+                            return '😔 ';
+                        // case 40:
+                        //  return '40';
+                        case 50:
+                            return '😐 ';
+                        // case 60:
+                        //  return '60';
+                        case 70:
+                            return '🙂 ';
+                        // case 80:
+                        //  return '80';
+                        case 90:
+                            return '😁 ';
+                        // case 100:
+                        //     return '100';
+                    }
+                }
+            },
+            gridLines: {
+              drawTicks: false,
+              display: false
+            }
+
+          }],
+          xAxes: [{
+            categories: ['1', '2', '3', '4', '5', '6', '7'],
+            gridLines: {
+              zeroLineColor: "transparent"
+            },
+            ticks: {
+              padding: 50,
+              fontColor: "rgba(0,0,0,0.5)",
+              fontStyle: "bold",
+              labelOffset: offset,
+            }
+          }]
+        }
+      },
+      tooltips: {
+            mode: "label"
+          }
+    });
+  }
+
   handlePlotRequest(event) {
     // alert('An essay was submitted: ' + this.state.value);
     // console.log("Submit clicked");
-    var response = fetch('http://localhost:5000/get_sentiment', {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'mode': 'no-cors',
-      }
+    
+    // var response = fetch('http://localhost:5000/get_sentiment', {
+    //   method: 'GET',
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Access-Control-Allow-Origin': '*',
+    //     'mode': 'no-cors',
+    //   }
+    // }).then(response => console.log(response.data));
+
+    // fetch('http://localhost:5000/get_sentiment').then(response => console.log(response.json().then(value => console.));
+    fetch('http://localhost:5000/get_sentiment').then(function(response) { 
+        return response.json();
+      }).then(function(myjson) {
+        //console.log(myjson['params']);
+        // this.plotData(myjson['params']);
+        // return myjson['params'];
+
+        var data_vals = myjson['params'];
+
+        var input = data_vals;
+    
+    // data_vals[input.length + 1] = input[input.length-1]
+    console.log(data_vals)
+    var ctx = document.getElementById('myChart').getContext("2d");
+    var rect = document.getElementById('myChart').getBoundingClientRect();
+    console.log(rect.top, rect.right, rect.bottom, rect.left);
+
+    var gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
+    gradientStroke.addColorStop(1, '#80b6f4');
+    gradientStroke.addColorStop(0, '#f49080');
+
+    var x_happy = new Image();
+    var happy = new Image();
+    var sad = new Image();
+    var x_sad = new Image();
+    var neutral = new Image();
+
+    x_happy.src = "/images/extreme-happy.png";
+    x_sad.src = "/images/extreme-sad.png";
+    sad.src = "/images/normal-sad.png";
+    happy.src = "/images/normal-happy.png";
+    neutral.src = "/images/neutral-face.png";
+    
+    var emotional_arr = [x_sad, sad, neutral, happy, x_happy]
+
+    //plot_points = ['rect', 'rect', x_happy, 'circle', 'rect', 'triangle', 'circle'];
+    // var chungus = new Image()
+  //    chungus.src ='chungus\ (1).png';
+      // chungus.style.width = '5px'
+      // chungus.style.height = '10px'
+    // var gradientFill = ctx.createLinearGradient(500, 0, 100, 0);
+
+    // plot_points = ["circle"]
+    var plot_points = []
+    for(var i = 0; i < input.length; i++) {
+      plot_points[i] = emotional_arr[parseInt((input[i]-1)/20)]
+    }
+    // plot_points[input.length+1] = "circle"
+    console.log(plot_points)
+    var gradientFill = ctx.createLinearGradient((rect.left+rect.right)/2, rect.top, (rect.left+rect.right)/2, rect.top + rect.bottom*3);
+    // gradientFill.addColorStop(0, "rgba(128, 182, 244, 0.6)");
+    // gradientFill.addColorStop(1, "rgba(244, 144, 128, 0.6)");
+
+
+    gradientFill.addColorStop(0, "rgba(244, 144, 128, 0.7)");
+    gradientFill.addColorStop(1, "rgba(128, 182, 244, 0.7)");
+
+    var offset = 50
+    Chart.pluginService.register({
+        afterUpdate: function(chart) {
+            // We get the dataset and set the offset here
+            var dataset = chart.config.data.datasets[0];
+
+            // For every data in the dataset ...
+            for (var i = 0; i < dataset._meta[0].data.length; i++) {
+                // We get the model linked to this data
+                var model = dataset._meta[0].data[i]._model;
+
+                // And add the offset to the `x` property
+                model.x += offset;
+
+                // .. and also to these two properties
+                // to make the bezier curve fits the new graph
+                model.controlPointNextX += offset;
+                model.controlPointPreviousX += offset;
+            }
+        }
     });
-    console.log(response);
+
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", ""],
+        // labels: ["f", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "f"],
+        datasets: [{
+          label: "Data",
+          borderColor: gradientStroke,
+          pointBorderColor: gradientStroke,
+          pointBackgroundColor: gradientStroke,
+          pointHoverBackgroundColor: gradientStroke,
+          pointHoverBorderColor: gradientStroke,
+          pointBorderWidth: 10,
+          pointHoverRadius: 10,
+          pointHoverBorderWidth: 1,
+          pointRadius: 3,
+          fill: true,
+          backgroundColor: gradientFill,
+          borderWidth: 10,
+          pointStyle: plot_points,
+          data: data_vals
+        }]
+      },
+      options: {
+        legend: {
+          position: "bottom"
+        },
+        layout: {
+              padding: {
+                  left: 50,
+                  right: 0,
+                  top: 0,
+                  bottom: 0
+              }
+          },
+        scales: {
+          yAxes: [{
+            ticks: {
+              fontColor: "rgba(0,0,0,0.5)",
+              fontStyle: "bold",
+              beginAtZero: true,
+              maxTicksLimit: 22,
+              padding: 60,
+              stepSize: 5,
+              fontSize: 60,
+              max: 100,
+              labelOffset: offset,
+              callback: function(label, index, labels) {
+                    switch (label) {
+                      // case 0:
+                      //  return ''
+                        case 10:
+                            return '😭 ';
+                        // case 20:
+                        //  return '';
+                        case 30:
+                            return '😔 ';
+                        // case 40:
+                        //  return '40';
+                        case 50:
+                            return '😐 ';
+                        // case 60:
+                        //  return '60';
+                        case 70:
+                            return '🙂 ';
+                        // case 80:
+                        //  return '80';
+                        case 90:
+                            return '😁 ';
+                        // case 100:
+                        //     return '100';
+                    }
+                }
+            },
+            gridLines: {
+              drawTicks: false,
+              display: false
+            }
+
+          }],
+          xAxes: [{
+            categories: ['1', '2', '3', '4', '5', '6', '7'],
+            gridLines: {
+              zeroLineColor: "transparent"
+            },
+            ticks: {
+              padding: 50,
+              fontColor: "rgba(0,0,0,0.5)",
+              fontStyle: "bold",
+              labelOffset: offset,
+            }
+          }]
+        }
+      },
+      tooltips: {
+            mode: "label"
+          }
+    });
+
+      });
+    // console.log(response);
+
+    // var fs = require('fs');
+    // fs.readFile("/Users/mananrai/Desktop/Treehacks/2019/ours/my-app/python/data_sentiment.json", (err, data) => { 
+    //     if (err) throw err; 
+      
+    //     console.log(data.toString()); 
+    // });
+    // console.log(data)
+    //this.plotData(data);
+
     event.preventDefault();
   }
 
@@ -97,7 +472,19 @@ class App extends React.Component {
     // console.log("Submit clicked");
     this.echo(this.state.value);
     var src = {text: this.state.value};
-    console.log(this.buildJSON(src));
+    var json = this.buildJSON(src);
+    console.log(json);
+    var response = fetch('http://localhost:5000/post_sentiment', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'mode': 'no-cors',
+      },
+      body: JSON.stringify(json.id)
+    });
+    console.log(response);
     event.preventDefault();
   }
 
@@ -117,7 +504,14 @@ class App extends React.Component {
             <div>
               <input type="submit" value="Submit" />
             </div>
+          </form> 
+          <form onSubmit={this.handlePlotRequest}>
+            <input type="submit" value="Plot" />
           </form>
+        </div>
+
+        <div class="map-wrap">
+          <canvas id="myChart"></canvas>
         </div>
 
         <div class="map-wrap">
